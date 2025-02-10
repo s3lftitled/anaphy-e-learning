df45517d22902c218e1fe49ffd9e09aaa76a60cb
frankiesochan <jjaviendemonteverde@gmail.com>
2025-02-10 14:09:18 +0800

    added landing page jsx and css

---
+++ client/src/pages/Landing/LandingPage.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import './LandingPage.css'
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="container">
+        <img src='1stpage.png' />
+      </div>
+
+      <div className="landing-nav">
+        <Link className="nav-link" href="">Home</Link>
+        <Link className="nav-link" href="">About</Link>
+        <Link className="nav-link" href="">Contact</Link>
+        <Link className="landing-login" to="/login">Login</Link>
+      </div>
+
+      <div className="glow glow-1"></div>
+      <div className="glow glow-2"></div>
+      <div className="glow glow-3"></div>
+
+      <h1 className="interato"> INTERACTIVE </h1>
+      <h1 className="humato"> HUMAN</h1>
+      <h1 className="anato"> ANATOMY </h1>
+      <hr>
+      </hr>
+      <h3> Enhance the educational experience for nursing students </h3>
+    </>
+  )
+}
+
+export default LandingPage
