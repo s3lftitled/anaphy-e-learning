36c060e2e75bef29fa2c9a1b19a688b2ef592aca
frankiesochan <jjaviendemonteverde@gmail.com>
2025-02-10 14:11:17 +0800

    refined error handler

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -2,13 +2,16 @@ const HTTP_STATUS = require('../constants/httpConstants')
 const logger = require('../logger/logger')
 
 const errorHandler = (err, req, res, next) => { 
-
   const statusCode = err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR
   const message = err.message || 'Something went wrong!'
 
-  logger.logError(err, req)
+  logger.error(`Error: ${message}`)
 
-  res.status(statusCode).json({ message })
+  res.status(statusCode).json({ 
+    success: false, 
+    message, 
+    errors: err.errors || null 
+  })
 }
 
 module.exports = errorHandler
