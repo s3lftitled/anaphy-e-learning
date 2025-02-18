25ee4211106bddabb8186d9abb903e7cbd21cdec
frankiesochan <jjaviendemonteverde@gmail.com>
2025-02-18 12:23:57 +0800

    added about us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,26 @@ const Login = lazy(() => import('./pages/Authentication/Login'))
 const Register = lazy(() => import('./pages/Authentication/Register'))
 const LandingPage = lazy(() => import('./pages/Landing/LandingPage'))
 const Verification = lazy(() => import('./pages/Authentication/Verification'))
-const Home = lazy(() => import('./pages/Home/Home'))
+const Homepage = lazy(() => import('./pages/Home/Home'))
+const AboutUs = lazy(() => import('./pages/AboutUs/AboutUs'))
+const TeacherManagement = lazy(() => import('./pages/TeacherManagement/TeacherManagement'))
+const ConfirmationForm = lazy(() => import('./components/ConfirmationForm'))
+
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}></Suspense>
-      <Routes>
-        <Route path='/' element={<LandingPage />}/>
-        <Route path='/home' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/verification/:email' element={<Verification />}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<LandingPage />}/>
+          <Route path='/home' element={<Homepage />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/verification/:email' element={<Verification />}/>
+          <Route path='/teacher-management' element={<TeacherManagement/>}/>
+          <Route path='/confirm-teacher-account' element={<ConfirmationForm/>}/>
+          <Route path='/about-us' element={<AboutUs/>}/>
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
