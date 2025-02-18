0a891cd9aa5fc51747cdb716021cb57635c9e5ba
frankiesochan <jjaviendemonteverde@gmail.com>
2025-02-18 10:17:53 +0800

    added method for sending confirmation link to teacher's email

--- server/src/utils/emailUtils.js
+++ server/src/utils/emailUtils.js
@@ -33,6 +33,27 @@ class EmailUtil {
       throw new Error('Failed to send verification email.')
     }
   }
+
+  async sendConfirmationEmail(sanitizedEmail, confirmationLink) {
+    try {
+
+      const mailOptions = {
+        from: process.env.USER,
+        to: sanitizedEmail,
+        subject: 'Teacher Account Confirmation',
+        html: `
+          <p>You have been added as a teacher to our system. Please confirm your account by clicking the link below:</p>
+          <p><a href="${confirmationLink}" target="_blank">Confirm My Account</a></p>
+          <p>If you did not request this, please ignore this email.</p>
+        `,
+      }
+
+      await this.transporter.sendMail(mailOptions)
+    } catch (error) {
+      logger.error('Error sending confirmation email:', error)
+      throw new Error('Failed to send confirmation email.')
+    }
+  }
 }
 
 module.exports = new EmailUtil()
