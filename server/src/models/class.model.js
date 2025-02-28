c171d09dc3e210537bd0ac4bcc2dff422bab3a12
frankiesochan <jjaviendemonteverde@gmail.com>
2025-02-28 14:14:10 +0800

    added more attributes

--- server/src/models/class.model.js
+++ server/src/models/class.model.js
@@ -4,15 +4,20 @@ const ClassSchema = new mongoose.Schema({
   name: { type: String, maxlength: 14, minlength: 6 },
   code: { type: String, unique: true },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  students: [
+    {
+      student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      status: {
+        type: String,
+        enum: ["invited", "rejected", "joined"],
+        default: "invited",
+      },
+    },
+  ],
   grades: [
     {
       student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      scores: [
-        {
-          type: Number, // Stores individual grades (quiz, assignment, etc.)
-        },
-      ],
+      scores: [{ type: Number }], // Stores individual grades (quiz, assignment, etc.)
       finalGrade: { type: Number, default: 0 }, // Stores overall class grade
     },
   ],
