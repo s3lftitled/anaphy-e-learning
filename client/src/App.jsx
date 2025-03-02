57718002d3d6f708f48926986a04c037f2688f7a
frankiesochan <jjaviendemonteverde@gmail.com>
2025-03-02 18:52:10 +0800

    added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
+import { AuthProvider } from './context/AuthContext'
 import './App.css'
 
 const Login = lazy(() => import('./pages/Authentication/Login'))
@@ -9,22 +10,31 @@ const Verification = lazy(() => import('./pages/Authentication/Verification'))
 const Homepage = lazy(() => import('./pages/Home/Home'))
 const TeacherManagement = lazy(() => import('./pages/TeacherManagement/TeacherManagement'))
 const ConfirmationForm = lazy(() => import('./components/ConfirmationForm'))
+const TeacherClassPage = lazy(() => import('./pages/Class/TeacherClassPage'))
+const TeacherDashboard = lazy(() => import('./pages/Class/TeacherDashboard'))
+const PersistLogin = lazy(() => import('./components/PersistLogin'))
 
 const App = () => {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<LandingPage />}/>
-          <Route path='/home' element={<Homepage />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-          <Route path='/verification/:email' element={<Verification />}/>
-          <Route path='/teacher-management' element={<TeacherManagement/>}/>
-          <Route path='/confirm-teacher-account' element={<ConfirmationForm/>}/>
-        </Routes>
-      </Suspense>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<LandingPage />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/verification/:email' element={<Verification />}/>
+            <Route element={<PersistLogin />}>
+              <Route path='/home' element={<Homepage />}/>
+              <Route path='/teacher-management' element={<TeacherManagement/>}/>
+              <Route path='/confirm-teacher-account' element={<ConfirmationForm/>}/>
+              <Route path='/teacher-class-page' element={<TeacherClassPage/>}/>
+              <Route path='/teacher-dashboard' element={<TeacherDashboard/>}/>
+            </Route>
+          </Routes>
+        </Suspense>
+     </Router>
+    </AuthProvider>
   )
 }
 
