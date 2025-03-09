81992b2acf17829668233cbdae098e7f85f45a42
frankiesochan <jjaviendemonteverde@gmail.com>
2025-03-09 10:21:54 +0800

    changed http status from ok to created

--- server/src/controllers/pageController.js
+++ server/src/controllers/pageController.js
@@ -11,7 +11,7 @@ async createPage(req, res, next) {
   const { lessonId } = req.params
   try {
     await createPageService(title, content, lessonId, link, order)
-    res.status(HTTP_STATUS.OK).json({ message: 'Page created succesfully' })
+    res.status(HTTP_STATUS.CREATED).json({ message: 'Page created succesfully' })
   } catch (error) {
     logger.error(`Pagecreation error - ${error.message}`)
     next(error)
