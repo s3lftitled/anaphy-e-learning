8f13f30e9dfb5a794e6db6fd133239c3581d809f
frankiesochan <jjaviendemonteverde@gmail.com>
2025-03-11 17:50:11 +0800

    e-learning page

---
+++ client/src/pages/E-Learning/E-LearningPage.jsx
@@ -0,0 +1,96 @@
+// ELearningPage.jsx
+import { useState } from 'react'
+import { useUser } from '../../context/UserContext'
+import Navbar from '../../components/Navbar/Navbar'
+import Sidebar from '../../components/LearningComponents/Sidebar'
+import ContentViewer from '../../components/LearningComponents/ContentViewer'
+import LoadingIndicator from '../../components/LearningComponents/LoadingIndicator'
+import ErrorMessage from '../../components/LearningComponents/ErrorMessage'
+import { useLearningData } from '../../hooks/useLearningData'
+import { useProgressTracking } from '../../hooks/useProgressTracking'
+import { useNavigation } from '../../hooks/useNavigation'
+
+const ELearningPage = () => {
+  const { user } = useUser()
+  const [sidebarExpanded, setSidebarExpanded] = useState(true)
+  
+  const { 
+    topics, 
+    currentTopic, 
+    setCurrentTopic, 
+    currentLesson,
+    setCurrentLesson, 
+    currentPage,
+    setCurrentPage,  
+    isLoading, 
+    error 
+  } = useLearningData(user)
+  
+  const {
+    completedContent,
+    markContentAsCompleted,
+    calculateProgress
+  } = useProgressTracking(user, currentTopic, currentLesson, currentPage)
+  
+  const {
+    expandedTopic,
+    expandedLesson,
+    handleTopicClick,
+    handleLessonClick,
+    handlePageClick,
+    handleNextPage,
+    handlePrevPage,
+  } = useNavigation(
+    topics, 
+    currentTopic, 
+    setCurrentTopic,
+    currentLesson,
+    setCurrentLesson,
+    currentPage,
+    setCurrentPage,
+    markContentAsCompleted
+  )
+
+  const toggleSidebar = () => {
+    setSidebarExpanded(!sidebarExpanded)
+  }
+
+  if (isLoading) return <LoadingIndicator />
+  if (error) return <ErrorMessage message={error} />
+
+  const progress = calculateProgress()
+
+  return (
+    <>
+      <Navbar user={user} />
+      <div className="e-learning-page">
+        <Sidebar 
+          topics={topics}
+          currentTopic={currentTopic}
+          currentLesson={currentLesson}
+          currentPage={currentPage}
+          expandedTopic={expandedTopic}
+          expandedLesson={expandedLesson}
+          sidebarExpanded={sidebarExpanded}
+          completedContent={completedContent}
+          progress={progress}
+          handleTopicClick={handleTopicClick}
+          handleLessonClick={handleLessonClick}
+          handlePageClick={handlePageClick}
+          toggleSidebar={toggleSidebar}
+        />
+        
+        <ContentViewer 
+          currentTopic={currentTopic}
+          currentLesson={currentLesson}
+          currentPage={currentPage}
+          handlePrevPage={handlePrevPage}
+          handleNextPage={handleNextPage}
+          isContentCompleted={contentId => completedContent.includes(contentId)}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ELearningPage
