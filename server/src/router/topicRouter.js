e7d95f4360741a3f5b41dc164b02a2ef0ee62e1e
frankiesochan <jjaviendemonteverde@gmail.com>
2025-03-18 10:09:32 +0800

    implemented jwt verify token for secured routes

--- server/src/router/topicRouter.js
+++ server/src/router/topicRouter.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const TopicController = require('../controllers/topicController')
+const { verifyToken } = require('../middlewares/jsonWebTokens')
 
-router.post('/v1/create-topic', TopicController.createTopic)
-router.get('/v1/fetch-all-topics', TopicController.fetchAllTopics)
-router.get('/v1/fetch-topic/:topicId', TopicController.fetchTopic)
-router.get('/v1/fetch-topic-lessons/:topicId', TopicController.fetchTopicLessons)
-router.get('/v1/fetch-topic-contents', TopicController.fetchTopicContents)
+router.post('/v1/create-topic', verifyToken, TopicController.createTopic)
+router.get('/v1/fetch-all-topics', verifyToken, TopicController.fetchAllTopics)
+router.get('/v1/fetch-topic/:topicId', verifyToken, TopicController.fetchTopic)
+router.get('/v1/fetch-topic-lessons/:topicId', verifyToken, TopicController.fetchTopicLessons)
+router.get('/v1/fetch-topic-contents', verifyToken, TopicController.fetchTopicContents)
 
 module.exports = router
