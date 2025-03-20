4e36032fa0d02e3536834fcff2f40b97a6b2abf9
frankiesochan <jjaviendemonteverde@gmail.com>
2025-03-21 06:41:31 +0800

    remove alerts and logs

--- client/src/pages/Authentication/Login/Login.jsx
+++ client/src/pages/Authentication/Login/Login.jsx
@@ -46,20 +46,18 @@ const Login = () => {
       )
 
       if (response.status === 200) {
-        alert(response.data.message)
-        console.log(response.data.accessToken)
         setAuth({ accessToken: response.data.accessToken})
         navigate('/home')
       }
     } catch (error) {
       if (error.response) {
         if (error.response.status === 429) {
-            alert('Too many requests. Please try again after 5 minutes');
+            alert('Too many requests. Please try again after 5 minutes')
         } else {
-            alert(error.response.data?.message || 'An error occurred. Please try again.');
+            alert(error.response.data?.message || 'An error occurred. Please try again.')
         }
       } else {
-          alert('An error occurred. Please check your network connection and try again.');
+          alert('An error occurred. Please check your network connection and try again.')
       }
     }
   }
